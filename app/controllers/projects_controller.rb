572efefdbfae78c9b6559064de2c87eab46e4f4f
sonle4 <sle4@cps.edu>
2016-04-25 18:08:54 +0000

    first commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,16 @@
+class ProjectsController < ApplicationController
+
+ def main 
+     render "main"
+ end 
+ 
+  def page1
+      render "page1"
+  end  
+  
+  def page2
+      render "page2"
+  end      
+
+
+end
